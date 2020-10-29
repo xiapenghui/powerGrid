c0dd5e19713d3bfc0115275e498ed2aa1e6cbec4
夏鹏辉 <7894900+xia_penghui@user.noreply.gitee.com>
2020-10-29 10:50:39 +0800

    空格

--- src/views/BusinessData/productionList.vue
+++ src/views/BusinessData/productionList.vue
@@ -10,6 +10,7 @@
           </el-col>
           <el-col :span="16"><el-input v-model="listQuery.ipoNo" :placeholder="$t('permission.ipoNoInfo')" clearable /></el-col>
         </el-col>
+
         <el-col :span="8">
           <el-col :span="8">
             <el-tooltip class="item" effect="dark" content="创建时间" placement="top-start"><label class="radio-label">创建时间:</label></el-tooltip>
