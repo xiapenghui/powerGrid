a0c00141c46c462ecb893c952af664a2d994a154
夏鹏辉 <7894900+xia_penghui@user.noreply.gitee.com>
2020-11-02 19:04:31 +0800

    服务器地址

--- src/api/Base.vue
+++ src/api/Base.vue
@@ -1,10 +1,10 @@
 <script>
-let BASE_URL = 'http://192.168.1.192:8888'; // 设置本地全局地址 URL
-// const BASE_URL = 'http://39.101.166.244' // 设置服务器全局地址 URL
+// let BASE_URL = 'http://192.168.1.192:8888'; // 设置本地全局地址 URL
+const BASE_URL = 'http://39.101.166.244' // 设置服务器全局地址 URL
 
 
-const IMG_URL = 'http://192.168.1.192:8888/api/image/upload'  //设置本地地址 URL
-// const IMG_URL = 'http://39.101.166.244/api/image/upload'  //设置服务器全局地址 URL
+// const IMG_URL = 'http://192.168.1.192:8888/api/image/upload'  //设置本地地址 URL
+const IMG_URL = 'http://39.101.166.244/api/image/upload'  //设置服务器全局地址 URL
 
 
 export default {
